638b641fc1103bb354f1e5cd16dc4ceeaba12f65
wjsdbsgus245 <wjsdbsgus245@naver.com>
2019-04-22 18:12:55 +0900

    0422 config edit

--- test.c
+++ test.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct data{
+    int i;
+    char *c;
+};
 
 int main(){
-    printf("Hello");
-     
+    struct data aData;
+
+    aData.i = 100;
+    aData.c = (char *)malloc(10);
+    strcpy(aData.c, "mwyun");
+
+    printf("aData.i = %d\n", aData.i);
+    printf("aData.c = %s\n", aData.c);
+
+    free(aData.c);
+
     return 0;
 }
--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 
-print("Hello")
+from urllib.request import urlopen
+from urllib.parse import quote_plus
+from bs4 import BeautifulSoup
+
+if __name__ == "__main__":
+    URL = "https://comic.naver.com/webtoon/weekday.nhn"
+    html = urlopen(URL)
+
+    soup = BeautifulSoup(html, "html.parser")
+    print(soup.decode('euc-kr'))
